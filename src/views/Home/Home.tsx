2a15001977b5a66bb03044f52001ad50b8925f67
Camilla Avila <avilacamilla@outlook.com>
2024-08-09 22:53:59 -0300

    feat: Chama novo componente HeaderHero na view Home e adiciona imagens mobile e desktop

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,18 +1,28 @@
-function Home() {
-    return(
-        <body className="home-container">
-            <header className="home-header-container">
-                Header aqui
-            </header>
-            <main className="home-main-container">
-                Main aqui
-            </main>
-            <footer className="home-footer-container">
-                Rodapé aqui
-            </footer>
-        </body>
-    );
-};
+// images
+import MobileBackgroundImage from '../../assets/images/mobile-header-hero.png';
+import DesktopBackgroundImage from '../../assets/images/desktop-header-hero.png';
+//styles
+import './home.css';
+//components
+import HeaderHero from '../../components/HeaderHero/HeaderHero';
 
+function Home() {
+  return (
+    <div className="home-container">
+      <header className="home-header-container">
+        <nav className="home-header-nav-container">
+          Barra de navegação
+        </nav>
+        <HeaderHero
+          mobileBackgroundImage={MobileBackgroundImage}
+          desktopBackgroundImage={DesktopBackgroundImage}
+          altText="Header Image"
+        />
+      </header>
+      <main className="home-main-container">Main aqui</main>
+      <footer className="home-footer-container">Rodapé aqui</footer>
+    </div>
+  );
+}
 
 export default Home;
